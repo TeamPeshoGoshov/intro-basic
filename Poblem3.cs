4ccc0f574d24f1c01bb7c91c43cc81dda7366889
Rosen <sasd123@abv.bg>
2018-03-12 18:31:12 +0200

    Problem 3

--- Poblem3.cs
+++ Poblem3.cs
@@ -6,7 +6,7 @@ namespace MilesToKilometers
     {
         static void Main(string[] args)
         {
-            //CONFIRMED from Insert000 
+            //CONFIRMED from - Insert000 
             double miles = double.Parse(Console.ReadLine()); //This is the input
 
             double kilometers = miles * 1.60934; // Here the user calculates,kilometers in miles
