c4d4a243b226fc5c2a0266ddd3aaf34f50ce51a2
Mihail Yordanov <shadow949494@gmail.com>
2018-03-12 17:53:51 +0200

    Add Problem1.cs

---
+++ Problem1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace DebitCardNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstNumber:D4} {secondNumber:D4} {thirdNumber:D4} {fourthNumber:D4}");
+        }
+    }
+}
