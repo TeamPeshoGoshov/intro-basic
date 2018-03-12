b8aa94d962f83fe65102647e14eb99fffa027c37
sabriM <sabrin400@abv.bg>
2018-03-12 18:11:35 +0200

    Add Problem 3

---
+++ Poblem3.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace MilesToKilometers
+{
+    class Problem3
+    {
+        static void Main(string[] args)
+        {
+            double miles = double.Parse(Console.ReadLine());
+
+            double kilometers = miles * 1.60934;
+
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
