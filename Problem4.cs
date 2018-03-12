9d934dbddf3396104e9f09556813f4b71ecc0e2b
sabriM <sabrin400@abv.bg>
2018-03-12 18:30:32 +0200

    Problem4.cs confirmed

--- Problem4.cs
+++ Problem4.cs
@@ -1,3 +1,4 @@
+//CONFIRMED from bloodwed12
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +11,19 @@ namespace ConsoleApplication36
     {
         static void Main(string[] args)
         {
+            //We recive from the Console the product name -> string;
             var product = Console.ReadLine();
+            /*In the next three lines, we recive volume,
+             * energy and sugar content of our products,
+             * as and integer */
             var volume = int.Parse(Console.ReadLine());
             var energyPer100ml = int.Parse(Console.ReadLine());
             var sugarPer100ml = int.Parse(Console.ReadLine());
-
+            //We calculate energy and sugar;
             double totalEnergy = energyPer100ml / 100.0 * volume;
             double totalSugar = sugarPer100ml * volume / 100.0;
 
+            //We print
             Console.WriteLine("{0}ml {1}:", volume, product);
             Console.WriteLine("{0}kcal, {1}g sugars", totalEnergy, totalSugar);
 
