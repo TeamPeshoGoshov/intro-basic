2a68be1e35bc5f3b13984a99a2d0f7dc3527eebe
Rosen <sasd123@abv.bg>
2018-03-13 12:19:54 +0200

    Add changes.

--- Problem 5/Program.cs
+++ Problem 5/Program.cs
@@ -8,11 +8,11 @@ namespace CharacterStats
         {    // We recieve from the console this ints and read them...
             string name = Console.ReadLine();
             int health = int.Parse(Console.ReadLine());
-            int healthMax = int.Parse(Console.ReadLine());
+            int healty = int.Parse(Console.ReadLine()); // changed healtyMax to healty :)
             int energy = int.Parse(Console.ReadLine());
             int energyMax = int.Parse(Console.ReadLine());
               //this strings merch by chars strings, to recieve the current health and energy
-            string currentHealt = "|" + new string('|', health) + new string('.', healthMax - health) + "|";
+            string currentHealt = "|" + new string('|', health) + new string('.', healty - health) + "|";
             string currentEnergy = "|" + new string('|', energy) + new string('.', energyMax - energy) + "|";
                 // We console print the name, currenthealt and currentEnergy...
             Console.WriteLine($"Name: {name}");
